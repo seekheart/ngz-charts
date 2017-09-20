25718aac29e9074c22fc7304784d6e5b896abdc5
Mike Tung <miketung2013@gmail.com>
2017-09-19 21:02:12 -0400

    #1 started working on the barchart, need to refactor and abstract methods out, and refine the chart visuals.
    For now the project is mainly bootstrapped

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  testData = [
+    {
+      'day': 1,
+      'gold': 1000
+    },
+    {
+      'day': 2,
+      'gold': 2000
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarchartModule} from './barchart/barchart.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BarchartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
