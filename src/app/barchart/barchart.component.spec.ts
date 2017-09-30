a8cb976880c13bcd1b21554e7f0ee249df0d2ed0
Mike Tung <miketung2013@gmail.com>
2017-09-30 12:33:55 -0400

    #2 need to figure out how to do testing

--- src/app/barchart/barchart.component.spec.ts
+++ src/app/barchart/barchart.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BarchartComponent } from './barchart.component';
+import {BarchartComponent} from './barchart.component';
+import {ChartService} from '../shared/chart.service';
 
 describe('BarchartComponent', () => {
-  let component: BarchartComponent;
-  let fixture: ComponentFixture<BarchartComponent>;
+  let comp;
+  let fixture;
+  let de;
+  let el;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BarchartComponent ]
-    })
-    .compileComponents();
-  }));
+  class MockChartService {
+  }
+
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [BarchartComponent],
+  //   }).compileComponents();
+  // }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BarchartComponent],
+      providers: [{provide: ChartService, useClass: MockChartService}]
+    });
+
     fixture = TestBed.createComponent(BarchartComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    comp = fixture.createComponent(BarchartComponent);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should make a container for my chart', () => {
+    expect(comp).toBeTruthy();
   });
 });
