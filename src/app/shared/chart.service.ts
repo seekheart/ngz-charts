ae2a8097242de5d3075a92bd8e2965a5f92cfc86
Mike Tung <miketung2013@gmail.com>
2017-10-05 18:22:22 -0400

    added docstrings to chart service which closes #2

--- src/app/shared/chart.service.ts
+++ src/app/shared/chart.service.ts
@@ -13,6 +13,16 @@ import { ElementRef, Injectable } from '@angular/core';
 @Injectable()
 export class ChartService {
 
+  /**
+   * Method to provide a chart canvas for d3 to operate on
+   *
+   * @param {object} element - reference html element to act on from template
+   * @param {number} width - width to make canvas in pixels
+   * @param {number} height - height to make canvas in pixels
+   * @param {object} margins - margins object to provide padding for drawing graph
+   *
+   * @return {object} d3 selection object to act on for drawing charts
+   */
   makeChartCanvas(element: ElementRef, width: number, height: number, margins: {}): d3.Selection<any, any, any, any> {
     const el = element.nativeElement;
 
