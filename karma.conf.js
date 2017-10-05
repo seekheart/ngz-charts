d04c1f599d16d8015b8304a66cb7d6764cd3ea55
Mike Tung <miketung2013@gmail.com>
2017-10-05 19:25:57 -0400

    #3 testing CI build plan with linting take 2

--- karma.conf.js
+++ karma.conf.js
@@ -19,12 +19,6 @@ module.exports = function (config) {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
     angularCli: {
       environment: 'dev'
     },
