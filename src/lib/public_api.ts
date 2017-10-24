941e2e875ef4e74b0fcf54aaa5a6f07a0baeb6a7
Mike Tung <miketung2013@gmail.com>
2017-10-24 19:05:36 -0400

    added tests and added timeline to public api. This closes #8

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -11,3 +11,4 @@ export * from './barChart/public_api';
 export * from './scatterPlot/public_api';
 export * from './shared/public_api';
 export * from './horizontal-bar-chart/public_api';
+export * from './timeline/public_api';
